a4f17dbefd7fe4b423ea371d0adaecfc47b33d61
Robert Tate <btate712@gmail.com>
2019-04-27 12:16:57 +0000

    Automatically backed up by Learn

--- db/migrate/20190427120922_figures.rb
+++ db/migrate/20190427120922_figures.rb
@@ -1,4 +1,23 @@
 class Figures < ActiveRecord::Migration
   def change
+    create_table :figures do |t|
+      t.string :name
+    end
+
+    create_table :titles do |t|
+      t.string :name
+    end
+
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+    end
+
+    create_table :title_figure do |t|
+      t.integer :title_id
+      t.integer :figure_id
+    end
   end
+
+  
 end
