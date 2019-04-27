c8b6d30cc4ca6362cfa50920971956780b91ca6b
Robert Tate <btate712@gmail.com>
2019-04-27 13:48:07 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,11 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
-    
+
     erb :'figures/new'
+  end
+
+  post '/figures' do
+    binding.pry
   end
 end
