cf9178a7a7f4e83709cafe7cf375676346647ce1
Robert Tate <btate712@gmail.com>
2019-04-27 12:48:26 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,7 +1,15 @@
 class LandmarksController < ApplicationController
   get '/landmarks/new' do
     @landmark = Landmark.new
-    
+
     erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+
+  end
+
+  post '/landmarks' do
+    binding.pry
   end
 end
